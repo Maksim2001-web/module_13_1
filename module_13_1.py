43465b9ce0a6d3dc8da00a6d24f202318d1f7571
Maksim2001-web <89614701484@mail.ru>
2024-11-03 13:30:42 +0300

    Update module_13_1.py

--- module_13_1.py
+++ module_13_1.py
@@ -8,10 +8,13 @@ async def start_strongman(name, power):
     print(f'Силач {name} закончил соревнования.')
 
 async def start_tournament():
-    await asyncio.gather(
-        start_strongman('Pasha', 3),
-        start_strongman('Denis', 4),
-        start_strongman('Apollon', 5)
-    )
+   
+    tasks = [
+        asyncio.create_task(start_strongman('Pasha', 3)),
+        asyncio.create_task(start_strongman('Denis', 4)),
+        asyncio.create_task(start_strongman('Apollon', 5))
+    ]
+    
+    await asyncio.gather(*tasks)
 
 asyncio.run(start_tournament())
